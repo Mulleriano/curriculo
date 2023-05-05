45a8006c8832bc9d68dc7be269c089bfd7501bc2
Igor Muller <igormullersantos@hotmail.com>
2023-05-05 11:39:46 -0300

    Teste finalizado

--- src/components/Carrossel.vue
+++ src/components/Carrossel.vue
@@ -1,26 +1,4 @@
 <script>
-
-export default {
-    data () {
-      return {
-        items: [
-          {
-            src: '../assets/minhaFoto.jpeg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: '../assets/minhaFoto.jpeg',
-          },
-          {
-            src: '../assets/minhaFoto.jpeg',
-          },
-        ],
-      }
-    },
-  }
-
 </script>
 
 <template>
@@ -38,14 +16,6 @@ export default {
         </div>
     </div> -->
 
-    <v-carousel hide-delimiters>
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-      cover
-    ></v-carousel-item>
-  </v-carousel>
 </template>
 
 <style scoped>
